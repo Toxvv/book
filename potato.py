ce609f8409e0a1dfcb904bb6062287183aa8fdba
HJW <1159624839@qq.com>
2024-03-24 19:26:13 +0800

    potato over

--- potato.py
+++ potato.py
@@ -1,1 +1,18 @@
 from pythonds import Queue
+def hotPotato(namelist,num):
+    simqueue=Queue.Queue()
+    for name in namelist:
+        simqueue.enqueue(name)
+
+    while simqueue.size()>1:
+        for i in range(num):
+            simqueue.enqueue(simqueue.dequeue())
+
+        simqueue.dequeue()
+    return simqueue.dequeue()
+
+namelist=['1','2','3','4','5','6']
+number=7
+
+final=hotPotato(namelist,number)
+print(final)
